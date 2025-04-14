655ad2c9ee75aa71f084ba587d83bc10f2fa3619
Lakhanahir9499 <lakhanlakhnotra@protonmail.com>
2025-04-14 22:58:54 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,39 +1,16 @@
 async function generateRoast() {
   const name = document.getElementById("name").value;
   const mood = document.getElementById("mood").value;
-  const resultDiv = document.getElementById("result");
-  const memeDiv = document.getElementById("meme");
-  const audio = document.getElementById("audio");
-
-  resultDiv.innerHTML = "Roasting...";
-  memeDiv.innerHTML = "";
-  audio.hidden = true;
-
-  const response = await fetch("/generate", {
+  const res = await fetch("/generate", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, mood }),
+    body: JSON.stringify({ name, mood })
   });
-
-  const data = await response.json();
-  if (data.roast) {
-    resultDiv.innerHTML = data.roast;
-    memeDiv.innerHTML = `<p class='text-lg'>\"${data.roast}\"</p>`;
-    if (data.audio) {
-      audio.src = "data:audio/mp3;base64," + data.audio;
-      audio.hidden = false;
-      audio.load();
-    }
-  } else {
-    resultDiv.innerHTML = "Error: " + data.error;
-  }
-}
-
-function saveMeme() {
-  html2canvas(document.getElementById("meme")).then((canvas) => {
-    const link = document.createElement("a");
-    link.download = "roast-meme.png";
-    link.href = canvas.toDataURL();
-    link.click();
-  });
+  const data = await res.json();
+  document.getElementById("output").innerHTML = `
+    <p>${data.roast}</p>
+    <audio controls src="data:audio/mp3;base64,${data.voice}"></audio>
+    <br/>
+    <img src="${data.meme}" width="300" />
+  `;
 }
